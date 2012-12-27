f4cb49f7ff182e7d3b229c1fa414865e8cda9c41
Marc Gilleron <zylann_sagas_mp3@hotmail.fr>
2012-12-27 02:58:47 +0100

    Tweaked heightmap generator (reduced Z offset)

--- src/zcraft/MapGenerator.cpp
+++ src/zcraft/MapGenerator.cpp
@@ -18,10 +18,37 @@ namespace zcraft
 		block = new Block(pos);
 
 		/*
+		// Generates suspended paths
+		if(pos.z == 0)
+		{
+			u8 x, y, z;
+			float n, nr;
+
+			for(z = 0; z < Block::SIZE; z++)
+			{
+				nr = 0.05f;
+
+				for(y = 0; y < Block::SIZE; y++)
+				for(x = 0; x < Block::SIZE; x++)
+				{
+					if(nr != 0)
+					{
+						n = noise2dPerlin(
+								x + org.x, y + org.y, m_seed, 5, 0.5, 128);
+						if(n > 0.5f-nr && n < 0.5f+nr)
+							block->set(x, y, z, Node(node::STONE));
+					}
+				}
+			}
+		}
+		*/
+
+		/*
 		float n;
 		float nr = 0.5f;
 		u8 x, y, z;
 
+		// Generates a gorgeous cave world
 		for(z = 0; z < Block::SIZE; z++)
 		{
 			if(org.z + z < 0)
@@ -31,8 +58,10 @@ namespace zcraft
 			else
 			{
 				nr = 1.f - static_cast<float>(org.z + z) / 256.f;
-				nr = nr * nr * nr;
+				nr = nr * nr * nr * nr;
 				nr /= 0.5f;
+				if(nr < 0.025f)
+					nr = 0.025f;
 			}
 
 			for(y = 0; y < Block::SIZE; y++)
@@ -45,25 +74,29 @@ namespace zcraft
 		}
 		*/
 
-		if(org.z >= 0 && org.z < 128)
+		// Generates a simple heightmap
+		if(org.z >= -48 && org.z < 80)
 		{
 			for(u8 y = 0; y < Block::SIZE; y++)
 			for(u8 x = 0; x < Block::SIZE; x++)
 			{
-				u16 h = (u16)(128.f * (float)noise2dPerlin(
-							x + org.x, y + org.y, m_seed, 5, 0.5, 128));
+				s16 h = (u16)(128.f * (float)noise2dPerlin(
+							x + org.x, y + org.y, m_seed, 5, 0.5, 128)) - 48;
 
 				if(h >= org.z)
 					h -= org.z;
 				else
 					h = 0;
 
+				if(h < 0)
+					std::cout << "e";
+
 				if(h >= Block::SIZE)
-					h = Block::SIZE - 1;
+					h = Block::SIZE;
 
 				if(h != 0)
 				{
-					for(u16 z = 0; z <= h; z++)
+					for(u16 z = 0; z < h; z++)
 						block->set(x, y, z, Node(node::STONE));
 				}
 			}
