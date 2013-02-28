9aa0d94c128a362636e843a530e27d4b9d51c022
Marc Gilleron <zylann_sagas_mp3@hotmail.fr>
2013-02-28 21:03:10 +0100

    Added new methods to Rect<T>

--- src/engine/Rect.hpp
+++ src/engine/Rect.hpp
@@ -41,6 +41,7 @@ namespace zn
 			max = max0;
 		}
 
+		// Initializes the rectangle from new edges
 		inline void set(const T minX, const T minY, const T maxX, const T maxY)
 		{
 			min.x = minX;
@@ -49,26 +50,28 @@ namespace zn
 			max.y = maxY;
 		}
 
+		// Initializes the rectangle from another
 		inline void set(const Rect & other)
 		{
 			min = other.min;
 			max = other.max;
 		}
 
+		// Initializes the rectangle from new edges
 		inline void set(const Vector2<T> & min0, const Vector2<T> & max0)
 		{
 			min = min0;
 			max = max0;
 		}
 
-		// Returns the positive width of th rectangle
+		// Returns the positive width of the rectangle
 		inline T width() const
 		{
 			const T w = max.x - min.x;
 			return w > 0 ? w : -w;
 		}
 
-		// Returns the positive height of th rectangle
+		// Returns the positive height of the rectangle
 		inline T height() const
 		{
 			const T h = max.y - min.y;
@@ -95,6 +98,26 @@ namespace zn
 			return contains(p.x, p.y);
 		}
 
+		// Tests if the given rectangle is included in the current one.
+		// Edges are included.
+		inline bool contains(const Rect<T> & other) const
+		{
+			return min.x <= other.min.x && max.x >= other.max.x &&
+					min.y <= other.min.y && max.y >= other.max.y ;
+		}
+
+		// Tests if the given rectangle intersects the current one.
+		// Edges are included.
+		inline bool intersects(const Rect<T> & other) const
+		{
+			return max.y >= other.min.y &&
+					min.y <= other.max.y &&
+					max.x >= other.min.x &&
+					max.x <= other.max.x ;
+		}
+
+		// TODO Rect: intersection
+
 		// Swaps min and max coordinates in order to keep them ordered
 		// (min.x <= max.x and min.y <= max.y)
 		inline void normalize()
@@ -106,6 +129,17 @@ namespace zn
 		}
 
 		// Offsets borders of the rectangle :
+		// negative value means contracting,
+		// positive value means expanding.
+		inline void pad(const T p)
+		{
+			min.x -= p;
+			min.y -= p;
+			max.x += p;
+			max.y += p;
+		}
+
+		// Offsets borders of the rectangle :
 		// negative values means contracting,
 		// positive values means expanding.
 		inline void pad(const T x, const T y)
@@ -114,6 +148,17 @@ namespace zn
 			min.y -= y;
 			max.x += x;
 			max.y += y;
+		}
+
+		// Offsets borders of the rectangle :
+		// negative values means contracting,
+		// positive values means expanding.
+		inline void pad(const T minx, const T miny, const T maxx, const T maxy)
+		{
+			min.x -= minx;
+			min.y -= miny;
+			max.x += maxx;
+			max.y += maxy;
 		}
 
 		// Moves the rectangle by a vector
@@ -132,16 +177,44 @@ namespace zn
 			max += off;
 		}
 
+		// Computes the center of the rectangle.
+		inline Vector2<T> getCenter() const
+		{
+			return Vector2<T>((min.x + max.y) / 2, (min.y + max.y) / 2);
+		}
+
 		// Moves the rectangle to have its origin at the given position
 		inline void setPositionFromOrigin(const T x, const T y)
 		{
 			offset(x - min.x, y - min.y);
 		}
 
-		// Moves the rectangle to have its origin at the given position
+		// Moves the rectangle to have its origin at the given position.
+		// The origin is the min point.
 		inline void setPositionFromOrigin(const Vector2<T> & p)
 		{
 			offset(p - min);
+		}
+
+		// Clips the rectangle to another.
+		// If they don't intersect, it will result in a rectangle with area = 0
+		inline void clipTo(const Rect<T> & other)
+		{
+			if(max.x > other.max.x)
+				max.x = other.max.x;
+			if(max.y > other.max.y)
+				max.y = other.max.y;
+
+			if(min.x > other.min.x)
+				min.x = other.min.x;
+			if(min.y > other.min.y)
+				min.y = other.min.y;
+
+			// correct possible invalid rect resulting from clipping
+			if(min.y > max.y)
+				min.y = max.y;
+			if(min.x > max.x)
+				min.x = max.x;
 		}
 
 	};
@@ -150,7 +223,8 @@ namespace zn
 	template <typename T>
 	std::ostream & operator<<(std::ostream & os, const Rect<T> & r)
 	{
-		os << "(" << r.min.x << ", " << r.min.y << ", " << r.max.x << ", " << r.max.y << ")";
+		os << "(" << r.min.x << ", " << r.min.y << ", "
+				  << r.max.x << ", " << r.max.y << ")";
 		return os;
 	}
 
