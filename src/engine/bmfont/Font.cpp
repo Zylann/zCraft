cb797fa2ecbf0fa940ebcc4f4cc38192c3820f49
Marc Gilleron <zylann_sagas_mp3@hotmail.fr>
2013-03-17 00:11:45 +0100

    Moved BMFont stuff into a single file

--- src/engine/bmfont/Font.cpp
+++ src/engine/bmfont/Font.cpp
@@ -19,6 +19,42 @@ namespace zn
 {
 namespace bmfont
 {
+	/*
+		FontSettings
+	*/
+
+	void FontSettings::addKerning(Kerning k)
+	{
+		m_kernings.push_back(k);
+	}
+
+	short FontSettings::getKerning(int first, int second) const
+	{
+		for(auto & k : m_kernings)
+		{
+			if(k.first == first && k.second != second)
+				return k.amount;
+		}
+		return 0;
+	}
+
+	void FontSettings::addChar(CharDescriptor cd)
+	{
+		m_chars[cd.id] = cd;
+	}
+
+	const CharDescriptor * FontSettings::getChar(int id) const
+	{
+		auto it = m_chars.find(id);
+		if(it != m_chars.end())
+			return &(it->second);
+		return nullptr;
+	}
+
+	/*
+		Font
+	*/
+
 	// TODO Font: handle accentued characters
 
 	Font::Font()
--- src/engine/bmfont/Font.hpp
+++ src/engine/bmfont/Font.hpp
@@ -4,23 +4,148 @@ Copyright (C) 2010-2012 Marc GILLERON
 This file is part of the zCraft project.
 */
 
-#ifndef ENGINE_BM_FONT_HPP_INCLUDED
-#define ENGINE_BM_FONT_HPP_INCLUDED
+#ifndef ZN_BM_FONT_HPP_INCLUDED
+#define ZN_BM_FONT_HPP_INCLUDED
 
 #include <SFML/Graphics.hpp>
 #include <vector>
-#include "engine/bmfont/FontSettings.hpp"
-#include "engine/bmfont/FontInfo.hpp"
+#include <list>
+#include <unordered_map>
+
+/*
+	This is a simple C++ OpenGL BMFont implementation.
+	Requires SFML, glew and C++11 compiler.
+	Documentation about BMFont : http://www.angelcode.com/products/bmfont/
+	(included in the installer provided on the website)
+*/
 
 namespace zn
 {
 namespace bmfont
 {
 	/*
-		This is a simple C++ OpenGL BMFont implementation.
-		Requires SFML, glew and C++11 compiler.
-		Documentation about BMFont : http://www.angelcode.com/products/bmfont/
-		(included in the installer provided on the website)
+		FontInfo gather informations about how the font was created
+	*/
+
+	struct FontInfo
+	{
+		struct Padding
+		{
+			int up = 0;
+			int right = 0;
+			int down = 0;
+			int left = 0;
+		};
+
+		struct Spacing
+		{
+			int horizontal = 0;
+			int vertical = 0;
+		};
+
+		std::string face; // This is the name of the true type font.
+		unsigned int size; // The size of the true type font.
+		bool bold; // The font is bold.
+		bool italic; // The font is italic.
+		std::string charset; // The name of the OEM charset used (when not unicode).
+		bool unicode; // Set to 1 if it is the unicode charset.
+		unsigned int stretchH; // The font height stretch in percentage. 100% means no stretch.
+		bool smooth; // Set to 1 if smoothing was turned on.
+		unsigned int aa; // The supersampling level used. 1 means no supersampling was used.
+		Padding padding; // The padding for each character (up, right, down, left).
+		Spacing spacing; // The spacing for each character (horizontal, vertical).
+		unsigned int outline; // The outline thickness for the characters.
+	};
+
+	struct Kerning
+	{
+		short first = 0; // The first character id.
+		short second = 0; // The second character id.
+
+		// How much the x position should be adjusted when drawing the
+		// second character immediately following the first.
+		short amount = 0;
+	};
+
+	struct CharDescriptor
+	{
+		short id = 0; //	The character id.
+
+		short x = 0; 	//	The left position of the character image in the texture.
+		short y = 0;	//	The top position of the character image in the texture.
+		short width = 0; //	The width of the character image in the texture.
+		short height = 0; //	The height of the character image in the texture.
+
+		//	How much the current position should be offset when copying the
+		// image from the texture to the screen.
+		short xoffset = 0;
+		short yoffset = 0;
+
+		// How much the current position should be advanced
+		// after drawing the character.
+		short xadvance = 0;
+
+		short page = 0; // The texture page where the character image is found.
+
+		// The texture channel where the character image is found
+		// (1 = blue, 2 = green, 4 = red, 8 = alpha, 15 = all channels).
+		short chnl = 15;
+
+	};
+
+	/*
+		FontSettings is what is used from the specification to draw the font.
+	*/
+
+	class FontSettings
+	{
+	private :
+
+		std::list<Kerning> m_kernings;
+		std::unordered_map<int, CharDescriptor> m_chars; // (id, char)
+
+	public :
+
+		std::vector<std::string> pages; // [id] = file
+
+		// This is the distance in pixels between each line of text.
+		unsigned short lineHeight = 0;
+
+		// The number of pixels from the absolute top of the line to the
+		// base of the characters.
+		unsigned short base = 0;
+
+		// The size of the texture, normally used to scale the coordinates of
+		// the character image.
+		unsigned short scaleW = 0;
+		unsigned short scaleH = 0;
+
+		// Set to 1 if the monochrome characters have been packed into each of
+		// the texture channels. In this case alphaChnl describes what is
+		// stored in each channel.
+		unsigned short packed = false;
+
+		// For each color :
+		// Set to 0 if the channel holds the glyph data, 1 if it holds the
+		// outline, 2 if it holds the glyph and the outline, 3 if its set to
+		// zero, and 4 if its set to one.
+		unsigned short alphaChnl = 0;
+		unsigned short redChnl = 4;
+		unsigned short greenChnl = 4;
+		unsigned short blueChnl = 4;
+
+	public :
+
+		void addKerning(Kerning k);
+		short getKerning(int first, int second) const;
+
+		void addChar(CharDescriptor cd);
+		const CharDescriptor * getChar(int id) const;
+
+	};
+
+	/*
+		The main font class
 	*/
 
 	class Font
@@ -66,6 +191,6 @@ namespace bmfont
 } // namespace bmfont
 } // namespace zn
 
-#endif // ENGINE_BM_FONT_HPP_INCLUDED
+#endif // ZN_BM_FONT_HPP_INCLUDED
 
 
