9cd41c7d0c4a9b7de19afd3afa7f0518d48ffa69
Marc Gilleron <zylann_sagas_mp3@hotmail.fr>
2013-03-31 20:44:46 +0200

    Update version

--- src/version.hpp
+++ src/version.hpp
@@ -4,7 +4,7 @@
 namespace autoversion{
 	
 	//Date Version Types
-	static const char DATE[] = "22";
+	static const char DATE[] = "31";
 	static const char MONTH[] = "03";
 	static const char YEAR[] = "2013";
 	static const char UBUNTU_VERSION_STYLE[] = "13.03";
@@ -16,17 +16,17 @@ namespace autoversion{
 	//Standard Version Type
 	static const long MAJOR = 0;
 	static const long MINOR = 1;
-	static const long BUILD = 309;
-	static const long REVISION = 1644;
+	static const long BUILD = 378;
+	static const long REVISION = 2030;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 413;
-	#define RC_FILEVERSION 0,1,309,1644
-	#define RC_FILEVERSION_STRING "0, 1, 309, 1644\0"
-	static const char FULLVERSION_STRING[] = "0.1.309.1644";
+	static const long BUILDS_COUNT = 510;
+	#define RC_FILEVERSION 0,1,378,2030
+	#define RC_FILEVERSION_STRING "0, 1, 378, 2030\0"
+	static const char FULLVERSION_STRING[] = "0.1.378.2030";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 309;
+	static const long BUILD_HISTORY = 378;
 	
 
 }
