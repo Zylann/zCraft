eb0cd74fee4c7c0df55abc4a27067243294201bd
Marc Gilleron <zylann_sagas_mp3@hotmail.fr>
2013-03-31 22:10:52 +0200

    Updated guards and head comment

--- src/engine/config.hpp
+++ src/engine/config.hpp
@@ -1,10 +1,16 @@
-#ifndef CONFIG_HPP_INCLUDED
-#define CONFIG_HPP_INCLUDED
+/*
+config.hpp
+Copyright (C) 2010-2013 Marc GILLERON
+This file is part of the zCraft project.
+*/
 
-#include "engine/system/osdefines.hpp"
+#ifndef ZN_CONFIG_HPP_INCLUDED
+#define ZN_CONFIG_HPP_INCLUDED
+
+#include "system/osdefines.hpp"
 
 // Indicates that we use SFML
-// Note : if disabled, the engine may not compile : some files are using
+// Note : if disabled, the engine may not compile : some files are using it
 // to ease SFML interfacing.
 #define ZN_SFML
 
@@ -14,4 +20,4 @@
 #define ZN_OPENGL2
 //#define ZN_OPENGL3
 
-#endif // CONFIG_HPP_INCLUDED
+#endif // ZN_CONFIG_HPP_INCLUDED
