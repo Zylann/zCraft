906ac01002d0bdb4008d55f7f62e05ea23b415a8
Marc Gilleron <zylann_sagas_mp3@hotmail.fr>
2013-05-09 02:39:59 +0200

    64-bit integer types and MSVC stuff

--- src/engine/core/types.hpp
+++ src/engine/core/types.hpp
@@ -11,14 +11,28 @@ This file is part of the zCraft project.
 
 namespace zn
 {
-	typedef unsigned int u32;
-	typedef unsigned short u16;
-	typedef unsigned char u8;
+    // 8 bits integer types
+    typedef signed char s8;
+    typedef unsigned char u8;
 
-	typedef int s32;
-	typedef short s16;
-	typedef char s8;
+    // 16 bits integer types
+    typedef signed short s16;
+    typedef unsigned short u16;
 
+    // 32 bits integer types
+    typedef signed int s32;
+    typedef unsigned int u32;
+
+	// 64 bits integer types
+#if defined(_MSC_VER)
+	typedef signed __int64 s64;
+	typedef unsigned __int64 u64;
+#else
+	typedef signed long long s64;
+	typedef unsigned long long u64;
+#endif
+	
+	// Float types
 	typedef float f32;
 	typedef double f64;
 }
