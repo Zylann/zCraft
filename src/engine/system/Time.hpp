257bf890bca80454a870972f0195800d1152b373
Marc Gilleron <zylann_sagas_mp3@hotmail.fr>
2013-05-14 00:37:03 +0200

    Better Time class

--- src/engine/system/Time.hpp
+++ src/engine/system/Time.hpp
@@ -7,49 +7,106 @@
 namespace zn
 {
 	// TODO Time: should be moved to core?
+	// TODO Time: create Clock class using <chrono>, and drop sf::Clock
+
 	class Time
 	{
-	private :
-
-		u32 m_millis;
-
 	public :
 
-		Time(u32 ms) : m_millis(ms)
+		// Constructs a time value from microseconds
+		Time(s64 micros) : m_micros(micros)
 		{}
 
 		// Returns the time value in seconds
-		inline f32 s() const
+		inline f32 seconds() const
 		{
-			return (f32)m_millis / 1000.f;
+			return (f32)m_micros / 10000000.f;
 		}
 
 		// Returns the time value in milliseconds
-		inline s32 ms() const
+		inline s64 millis() const
 		{
-			return m_millis;
+			return m_micros / 1000;
+		}
+
+		// Returns the time value in microseconds
+		inline s64 micros() const
+		{
+			return m_micros;
 		}
 
 		// Returns the time frequency in hertz.
 		// Returns -1 if the time is 0.
 		inline f32 hz() const
 		{
-			const float seconds = s();
-			if(seconds != 0)
-				return 1.f / seconds;
+			const float s = seconds();
+			if(s > 0)
+				return 1.f / s;
 			return -1;
 		}
 
-		// NOT USED, because using time spans is better than getting the clock().
-//		static inline u32 millis()
-//		{
-//			std::chrono::time_point_cast<
-//					std::chrono::milliseconds>(
-//						std::chrono::high_resolution_clock::now().time_point);
-//		}
+		inline Time & operator+=(const Time & other)
+		{
+			m_micros += other.micros();
+			return *this;
+		}
 
-		// TODO Time: create Timer class using <chrono>, and drop sf::Clock
+		inline Time & operator-=(const Time & other)
+		{
+			m_micros -= other.micros();
+			return *this;
+		}
+
+		inline Time & operator*=(f32 s)
+		{
+			m_micros = seconds() * s / 1000000.f;
+			return *this;
+		}
+
+		inline Time & operator/=(f32 s)
+		{
+			m_micros = seconds() / s / 1000000.f;
+			return *this;
+		}
+
+		// Returns a time from seconds
+		static inline Time seconds(f32 s)
+		{
+			return Time(s * 1000000.f);
+		}
+
+		// Returns a time from microseconds
+		static inline Time micros(s64 ms)
+		{
+			return Time(ms);
+		}
+
+		// Returns a time from milliseconds
+		static inline Time millis(s64 ms)
+		{
+			return Time(ms * 1000);
+		}
+
+	private :
+
+		// Note : s64 is enough to store 292 471 years,
+		// negative or positive, using microseconds.
+		s64 m_micros;
 	};
+
+	inline Time operator==(const Time & a, const Time & b)	{ return a.micros() == b.micros(); }
+	inline Time operator!=(const Time & a, const Time & b)	{ return a.micros() == b.micros(); }
+	inline Time operator>(const Time & a, const Time & b)	{ return a.micros() > b.micros(); }
+	inline Time operator<(const Time & a, const Time & b)	{ return a.micros() < b.micros(); }
+
+	inline Time operator+(const Time & a, const Time & b) 	{ return Time(a.micros() + b.micros()); }
+	inline Time operator-(const Time & a, const Time & b)	{ return Time(a.micros() - b.micros()); }
+
+	inline Time operator*(const Time & a, s64 b)	{ return Time(a.micros() * b); }
+	inline Time operator*(const Time & a, f32 b)	{ return Time::seconds(a.seconds() * b); }
+	inline Time operator/(const Time & a, s64 b)	{ return Time(a.micros() / b); }
+	inline Time operator/(const Time & a, f32 b)	{ return Time::seconds(a.seconds() / b); }
+
 
 } // namespace zn
 
